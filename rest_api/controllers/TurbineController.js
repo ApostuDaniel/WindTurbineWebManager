3605e33f29814ecc9f49464d400f5d15b132b6c0
Apostu Daniel <apostudanbs@gmail.com>
2022-06-13 20:39:40 +0300

    Changed corse for all turbines get to allow same origin

--- rest_api/controllers/TurbineController.js
+++ rest_api/controllers/TurbineController.js
@@ -3,9 +3,26 @@ const mongoose = require('mongoose')
 const Turbine = require('./../schemas/Turbine')
 const AllTurbineData = require('./../schemas/AllTurbineData')
 
+// @desc    Gets All turbines
+// @route   GET /api/turbines
 async function getTurbines(req, res) {
   try {
     const turbines = await Turbine.find()
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    })
+    res.end(JSON.stringify(turbines))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// @desc    Gets All turbines
+// @route   GET /api/turbines
+async function getPublicTurbines(req, res) {
+  try {
+    const turbines = await Turbine.find({})
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify(turbines))
   } catch (error) {
