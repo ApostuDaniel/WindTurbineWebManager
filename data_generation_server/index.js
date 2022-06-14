f4bb978d1343674b09bb3fdf9370b6e66d95de63
dalvagon <75310583+dalvagon@users.noreply.github.com>
2022-06-14 19:22:22 +0300

    called the weather api on turbine

--- data_generation_server/index.js
+++ data_generation_server/index.js
@@ -16,12 +16,28 @@ async function updateTurbines()
         const turbines = await getTurbines();
 
         for(turbine of turbines) {
-            const id = turbine._id;
-            console.log(id);
+            await updateTurbine(turbine);
+            break;
         }
 
-        await new Promise(resolve => setTimeout(resolve, 5000));
+        await new Promise(resolve => setTimeout(resolve, 500));
     }
+}
+
+async function updateTurbine(turbine) {
+    const lat = turbine.latitude;
+    const lng = turbine.longitude;
+
+    const weather_api_url = `http://api.weatherapi.com/v1/current.json?key=2407cb95cd0e4b31971101252221306&q=${lat},${lng}&aqi=no`;
+    const response = await fetch(weather_api_url);
+    const json = await response.json();
+
+    const locat = json.location.country + ', ' + json.location.region + ', ' + json.location.name;
+    const wind = json.current.wind_mph;
+    const temp = json.current.temp_c;
+    const hum = json.current.humidity;
+
+    console.log(locat);
 }
 
 updateTurbines();
