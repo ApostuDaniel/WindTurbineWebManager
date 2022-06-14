fae7d5eaf966abf8cf64701936562e5aa425956a
raresbancescu <75622881+raresbancescu@users.noreply.github.com>
2022-06-14 17:40:54 +0300

    Solved owned and public pages

--- web_app/controllers/PageController.js
+++ web_app/controllers/PageController.js
@@ -19,6 +19,7 @@ async function getPublicPage(req, res) {
       filename: 'public.ejs',
       turbines: turbineData,
     })
+    
     res.end(htmlRenderized)
   } catch (error) {
     console.log(error.message)
@@ -41,6 +42,7 @@ async function getPrivatePage(req, res) {
       filename: 'owned.ejs',
       turbines: ownedTurbineData,
     })
+    
     res.end(htmlRenderized)
   } catch (error) {
     console.log(error.message)
