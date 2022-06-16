d3c4a2561c33308e544804e658f2e1045243fabf
dalvagon <75310583+dalvagon@users.noreply.github.com>
2022-06-16 17:04:11 +0300

    trying to call put endpoint

--- data_generation_server/index.js
+++ data_generation_server/index.js
@@ -46,6 +46,8 @@ async function updateTurbines()
 async function stopTurbine(turbine) {
     const id = turbine._id;
 
+    console.log('Turbine old: ');
+    console.log(turbine);
     const date = Date.now();
     var newData = {
         "windSpeed": 0,
@@ -63,40 +65,68 @@ async function stopTurbine(turbine) {
  * @param {JSON} turbine 
  */
 async function updateTurbine(turbine) {
+    var newData;
     const id = turbine._id;
+
+    const lat = turbine.latitude;
+    const lng = turbine.longitude;
     const turbine_latest_data_api_url = `http://localhost:5000/api/turbines/data/${id}/new`;
 
     const data = await fetch(turbine_latest_data_api_url);
     const turbineData = await data.json();
 
+    console.log('Turbine old: ');
     console.log(turbineData);
 
-    const lat = turbine.latitude;
-    const lng = turbine.longitude;
-    const oldWindSpeed = turbineData.windSpeed;
-    const oldTurbineWear = turbineData.turbineWear;
-    const oldPowerGenerated = turbineData.powerGenerated;
-    const oldEfficiency = turbineData.eficiency;
+    if(turbineData.message === null) {
+        const oldWindSpeed = turbineData.windSpeed;
+        const oldTurbineWear = turbineData.turbineWear;
+        const oldPowerGenerated = turbineData.powerGenerated;
+        const oldEfficiency = turbineData.eficiency;
 
-    const weather_api_url = `http://api.weatherapi.com/v1/current.json?key=2407cb95cd0e4b31971101252221306&q=${lat},${lng}&aqi=no`;
-    const response = await fetch(weather_api_url);
-    const json = await response.json();
+        const weather_api_url = `http://api.weatherapi.com/v1/current.json?key=2407cb95cd0e4b31971101252221306&q=${lat},${lng}&aqi=no`;
+        const response = await fetch(weather_api_url);
+        const json = await response.json();
 
-    const currentWindSpeed = json.current.wind_mph;
-    const currentTemperature = json.current.temp_c;
-    const currentHummidity = json.current.humidity;
+        const currentWindSpeed = json.current.wind_mph;
+        const currentTemperature = json.current.temp_c;
+        const currentHummidity = json.current.humidity;
 
+        const date = Date.now();
+        const newWindSpeed = getNewWindSpeed(oldWindSpeed, currentWindSpeed);
+        const newTurbineWear = getNewTurbineWear(oldTurbineWear, newWindSpeed, currentTemperature, currentHummidity);
+        const newPowerGenerated = getNewPowerGenerated(oldPowerGenerated, newWindSpeed);
+        const newEffieciency = getNewEfficiency(oldEfficiency, oldPowerGenerated, newPowerGenerated);
 
-    const date = Date.now();
-    const newWindSpeed = getNewWindSpeed(oldWindSpeed, currentWindSpeed);
-    const newTurbineWear = getNewTurbineWear(oldTurbineWear, newWindSpeed, currentTemperature, currentHummidity);
-    const newPowerGenerated = getNewPowerGenerated(oldPowerGenerated, newWindSpeed);
-    var newData = {
-        "windSpeed": newWindSpeed,
-        "turbineWear": newTurbineWear,
-        "powerGenerated": newPowerGenerated,
-        "eficiency": oldEfficiency + 1,
-        "timeStamp": date.valueOf()
+        newData = {
+            "windSpeed": newWindSpeed,
+            "turbineWear": newTurbineWear,
+            "powerGenerated": newPowerGenerated,
+            "eficiency": newEffieciency,
+            "timeStamp": date.valueOf()
+        }
+    } else {
+        const weather_api_url = `http://api.weatherapi.com/v1/current.json?key=2407cb95cd0e4b31971101252221306&q=${lat},${lng}&aqi=no`;
+        const response = await fetch(weather_api_url);
+        const json = await response.json();
+
+        const currentWindSpeed = json.current.wind_mph;
+        const currentTemperature = json.current.temp_c;
+        const currentHummidity = json.current.humidity;
+
+        const date = Date.now();
+        const newWindSpeed = currentWindSpeed;
+        const newTurbineWear = 0;
+        const newPowerGenerated = currentWindSpeed;
+        const newEffieciency = 0.5;
+
+        newData = {
+            "windSpeed": newWindSpeed,
+            "turbineWear": newTurbineWear,
+            "powerGenerated": newPowerGenerated,
+            "eficiency": newEffieciency,
+            "timeStamp": date.valueOf()
+        }
     }
 
     await postNewData(id, newData);
@@ -110,36 +140,40 @@ function getNewTurbineWear(oldTurbineWear, windSpeed, temperature, humidity) {
     let newTurbineWear = oldTurbineWear;
 
     if(windSpeed > 50) {
-        newTurbineWear += 0.3;
-    }
-
-    if(temperature > 30) {
         newTurbineWear += 0.1;
     }
 
+    if(temperature > 30) {
+        newTurbineWear += 0.05;
+    }
+
     if(humidity > 60) {
-        newTurbineWear += 0.2;
+        newTurbineWear += 0.04;
     }
 
     return newTurbineWear;
 }
 
 function getNewPowerGenerated(oldPowerGenerated, windSpeed) {
-    return oldPowerGenerated + windSpeed;
+    return oldPowerGenerated + windSpeed / 10;
 }
 
-// function getNewEfficiency() {
-//     reutrn 
-// }
+function getNewEfficiency(oldEfficiency, oldPowerGenerated, newPowerGenerated) {
+    return (newPowerGenerated - oldPowerGenerated > 20) ? oldEfficiency + 0.01 : oldEfficiency - 0.01;
+}
 
 async function postNewData(id, newData) {
-    console.log(JSON.stringify(newData));
-    // const turbine_post_new_data_api_url = `http://localhost:5000/turbines/newdata/${id}`;
-    // await fetch(turbine_post_new_data_api_url, {
-    //     method: "POST",
-    //     body: JSON.stringify(newData),
-    //     headers: { "Content-Type": "application/json" }
-    // });
+    console.log('Turbine new: ');
+    console.log(newData);
+    const turbine_post_new_data_api_url = `http://localhost:5000/turbines/newdata/${id}`;
+    const response = await fetch(turbine_post_new_data_api_url, {
+        method: "PUT",
+        mode: "cors",
+        body: JSON.stringify(newData),
+        headers: { "Content-Type": "application/json" }
+    });
+
+    console.log(response);
 }
 
 updateTurbines();
