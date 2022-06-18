83dd974df8d48097914a658a2cf35a8bb9a1f40d
dalvagon <75310583+dalvagon@users.noreply.github.com>
2022-06-18 14:34:28 +0300

    added idfrom cookie to turbine creation page

--- rest_api/controllers/TurbineController.js
+++ rest_api/controllers/TurbineController.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
-const Turbine = require('./../schemas/Turbine')
-const AllTurbineData = require('./../schemas/AllTurbineData')
-const User = require('./../schemas/User')
-const Alert = require('./../schemas/Alert')
-const Notification = require('./../schemas/Notification')
+const mongoose = require("mongoose");
+const Turbine = require("./../schemas/Turbine");
+const AllTurbineData = require("./../schemas/AllTurbineData");
+const User = require("./../schemas/User");
+const Alert = require("./../schemas/Alert");
+const Notification = require("./../schemas/Notification");
 
-const { getRequestData, validateJSON } = require('../utils')
+const { getRequestData, validateJSON } = require("../utils");
 
 //GET
 
@@ -13,13 +13,13 @@ const { getRequestData, validateJSON } = require('../utils')
 // @route   GET /api/turbines
 async function getTurbines(req, res) {
   try {
-    const turbines = await Turbine.find()
+    const turbines = await Turbine.find();
     res.writeHead(200, {
-      'Content-Type': 'application/json',
-    })
-    res.end(JSON.stringify(turbines))
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify(turbines));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -27,16 +27,16 @@ async function getTurbines(req, res) {
 // @route   GET /api/turbines/:id
 async function getTurbine(req, res, id) {
   try {
-    const turbine = await Turbine.findById(id)
+    const turbine = await Turbine.findById(id);
     if (turbine) {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(turbine))
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(turbine));
     } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: `Turbine with id ${id} not found` }))
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: `Turbine with id ${id} not found` }));
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -44,11 +44,11 @@ async function getTurbine(req, res, id) {
 // @route   GET /api/turbines/public
 async function getPublicTurbines(req, res) {
   try {
-    const publicTurbines = await Turbine.find({ isPublic: true })
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(publicTurbines))
+    const publicTurbines = await Turbine.find({ isPublic: true });
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(publicTurbines));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -58,11 +58,11 @@ async function getPrivateTurbines(req, res, userId) {
   try {
     const privateTurbines = await Turbine.find({
       userId: userId,
-    })
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(privateTurbines))
+    });
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(privateTurbines));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -71,23 +71,23 @@ async function getPrivateTurbines(req, res, userId) {
 async function getPrivateTurbineByName(req, res, userId, name) {
   try {
     const privateTurbine = await Turbine.findOne({
-      name: new RegExp('^' + name + '$', 'i'),
+      name: new RegExp("^" + name + "$", "i"),
       userId: userId,
-    })
+    });
 
     if (!privateTurbine) {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.writeHead(404, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({
           message: `Private Turbine with name ${name} not found`,
         })
-      )
+      );
     } else {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(privateTurbine))
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(privateTurbine));
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -96,23 +96,23 @@ async function getPrivateTurbineByName(req, res, userId, name) {
 async function getPublicTurbineByName(req, res, name) {
   try {
     const publicTurbine = await Turbine.findOne({
-      name: new RegExp('^' + name + '$', 'i'),
+      name: new RegExp("^" + name + "$", "i"),
       isPublic: true,
-    })
+    });
 
     if (!publicTurbine) {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.writeHead(404, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({
           message: `Public Turbine with name ${name} not found`,
         })
-      )
+      );
     } else {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(publicTurbine))
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(publicTurbine));
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -120,18 +120,18 @@ async function getPublicTurbineByName(req, res, name) {
 // @route   GET /api/turbines/data/:id
 async function getTurbineData(req, res, id) {
   try {
-    const turbineData = await AllTurbineData.findOne({ turbineId: id })
+    const turbineData = await AllTurbineData.findOne({ turbineId: id });
     if (turbineData) {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(turbineData))
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(turbineData));
     } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.writeHead(404, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({ message: `No Turbine Data for turbine id ${id}` })
-      )
+      );
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -139,23 +139,23 @@ async function getTurbineData(req, res, id) {
 // @route   GET /api/turbines/data/:id/new
 async function getNewestTurbineData(req, res, id) {
   try {
-    const turbineData = await AllTurbineData.findOne({ turbineId: id })
+    const turbineData = await AllTurbineData.findOne({ turbineId: id });
     if (
       turbineData &&
       turbineData.historicData != null &&
       turbineData.historicData.length > 0
     ) {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.writeHead(200, { "Content-Type": "application/json" });
 
-      res.end(JSON.stringify(turbineData.historicData.slice(-1)[0]))
+      res.end(JSON.stringify(turbineData.historicData.slice(-1)[0]));
     } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.writeHead(404, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({ message: `No Turbine Data for turbine id ${id}` })
-      )
+      );
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -166,13 +166,13 @@ async function getNewestTurbineData(req, res, id) {
 
 async function createTurbine(req, res) {
   try {
-    const textBody = await getRequestData(req)
-    const jsonBody = validateJSON(textBody)
+    const textBody = await getRequestData(req);
+    const jsonBody = validateJSON(textBody);
 
     if (!jsonBody) {
-      res.writeHead(400, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Invalid request JSON' }))
-      return
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Invalid request JSON" }));
+      return;
     }
 
     const {
@@ -187,18 +187,18 @@ async function createTurbine(req, res) {
       suitability,
       isPublic,
       turbineState,
-    } = jsonBody
+    } = jsonBody;
 
-    const user = await User.findById(userId)
+    const user = await User.findById(userId);
 
     if (user == null) {
-      res.writeHead(422, { 'Content-Type': 'application/json' })
+      res.writeHead(422, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({
           message: `The referenced userId ${userId} doesn't exist in the database`,
         })
-      )
-      return
+      );
+      return;
     }
 
     try {
@@ -214,21 +214,21 @@ async function createTurbine(req, res) {
         suitability,
         isPublic,
         turbineState,
-      })
+      });
 
       const turbineData = await AllTurbineData.create({
         turbineId: turbine._id,
         historicData: [],
-      })
+      });
 
-      res.writeHead(201, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(turbine))
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(turbine));
     } catch (error) {
-      res.writeHead(400, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: error.message }))
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: error.message }));
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -240,51 +240,51 @@ async function createTurbine(req, res) {
 
 async function postNewData(req, res, id) {
   try {
-    const textBody = await getRequestData(req)
-    const jsonBody = validateJSON(textBody)
+    const textBody = await getRequestData(req);
+    const jsonBody = validateJSON(textBody);
 
     if (!jsonBody) {
-      res.writeHead(400, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Invalid request JSON' }))
-      return
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Invalid request JSON" }));
+      return;
     }
 
-    let turbineData = await AllTurbineData.findOne({ turbineId: id })
-    const turbine = await Turbine.findById(id)
+    let turbineData = await AllTurbineData.findOne({ turbineId: id });
+    const turbine = await Turbine.findById(id);
 
     if (turbine === null) {
-      res.writeHead(422, { 'Content-Type': 'application/json' })
+      res.writeHead(422, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({
           message: `The referenced turbine with id ${id} doesn't exist in the database`,
         })
-      )
-      return
+      );
+      return;
     } else if (turbineData === null) {
       turbineData = await AllTurbineData.create({
         turbineId: turbine._id,
         historicData: [],
-      })
+      });
     }
 
     try {
-      const newTurbineData = jsonBody
+      const newTurbineData = jsonBody;
 
       if (turbineData.historicData.length >= 144) {
-        turbineData.historicData.shift()
+        turbineData.historicData.shift();
       }
 
-      turbineData.historicData.push(newTurbineData)
-      await turbineData.save()
+      turbineData.historicData.push(newTurbineData);
+      await turbineData.save();
 
-      res.writeHead(201, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(turbineData.historicData.slice(-1)[0]))
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(turbineData.historicData.slice(-1)[0]));
     } catch (error) {
-      res.writeHead(400, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: error.message }))
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: error.message }));
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
@@ -293,60 +293,60 @@ async function postNewData(req, res, id) {
 
 async function updateTurbine(req, res, id) {
   try {
-    const textBody = await getRequestData(req)
-    const jsonBody = validateJSON(textBody)
+    const textBody = await getRequestData(req);
+    const jsonBody = validateJSON(textBody);
 
     if (!jsonBody) {
-      res.writeHead(400, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Invalid request JSON' }))
-      return
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Invalid request JSON" }));
+      return;
     }
 
-    const turbine = await Turbine.findById(id)
+    const turbine = await Turbine.findById(id);
 
     if (turbine == null) {
-      res.writeHead(422, { 'Content-Type': 'application/json' })
+      res.writeHead(422, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({
           message: `Turbine with id ${id} doesn't exist in the database`,
         })
-      )
-      return
+      );
+      return;
     }
 
     try {
       for (prop in turbine) {
-        turbine[prop] = jsonBody[prop] ?? turbine[prop]
+        turbine[prop] = jsonBody[prop] ?? turbine[prop];
       }
 
-      await turbine.save()
+      await turbine.save();
 
-      res.writeHead(201, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(turbine))
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(turbine));
     } catch (error) {
-      res.writeHead(400, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: error.message }))
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: error.message }));
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 async function deleteTurbine(req, res, id) {
   try {
-    await helperDeleteTurbineRelatedData(id)
-    res.writeHead(204, { 'Content-Type': 'application/json' })
-    res.end()
+    await helperDeleteTurbineRelatedData(id);
+    res.writeHead(204, { "Content-Type": "application/json" });
+    res.end();
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 async function helperDeleteTurbineRelatedData(id) {
-  await AllTurbineData.findOneAndDelete({ turbineId: id })
-  await Alert.deleteMany({ idTurbine: id })
-  await Notification.deleteMany({ idTurbine: id })
-  await Turbine.findByIdAndDelete(id)
+  await AllTurbineData.findOneAndDelete({ turbineId: id });
+  await Alert.deleteMany({ idTurbine: id });
+  await Notification.deleteMany({ idTurbine: id });
+  await Turbine.findByIdAndDelete(id);
 }
 
 module.exports = {
@@ -363,4 +363,4 @@ module.exports = {
   updateTurbine,
   helperDeleteTurbineRelatedData,
   deleteTurbine,
-}
+};
