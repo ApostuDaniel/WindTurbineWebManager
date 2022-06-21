4f3bc47d0361b4e3c975ca3f6fb72d22a945e153
dalvagon <75310583+dalvagon@users.noreply.github.com>
2022-06-21 15:15:03 +0300

    Landing page redesign

--- web_app/controllers/PageController.js
+++ web_app/controllers/PageController.js
@@ -37,7 +37,8 @@ async function getPublicPage(req, res, id) {
     const turbines = [];
 
     for (turbineData of turbinesData) {
-      const location = await restAPIInteraction.getLocation(turbineData);
+      // const location = await restAPIInteraction.getLocation(turbineData);
+      const location = 'Not shown yet';
       turbines.push({
         turbineData, 
         location
